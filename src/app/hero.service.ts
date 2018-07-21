28b4753f52fb99596a1415d944312b9501096954
PTAdrian1997 <poptifreaadrian@gmail.com>
2018-07-21 11:11:18 +0300

    - Created the getHero method: gets a hero by id;

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -48,7 +48,14 @@ export class HeroService {
   }
   
   getHero(id: number): Observable<Hero> {
-	  return of(HEROES.find(hero => hero.id == id));
+	  //return of(HEROES.find(hero => hero.id == id));
+	  const url = `${this.heroesUrl}/${id}`;
+	  return this.http.get<Hero>(url)
+		.pipe(
+			tap(_ => this.log(`fetched hero id=${id}`)),
+			catchError(this.handleError<Hero>(`getHero id = ${id}`))
+		)
+	  ;
   }
   
   private log(message: string): void {
